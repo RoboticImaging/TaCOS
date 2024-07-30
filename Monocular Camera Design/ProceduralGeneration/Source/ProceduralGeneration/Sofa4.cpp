a3a126a9b1a14d911d8e2211ccbd513f370d29b1
Oliver Yan <64511896+OYan60718@users.noreply.github.com>
2024-07-30 16:17:24 +1000

    Update Sofa4.cpp

--- Monocular Camera Design/ProceduralGeneration/Source/ProceduralGeneration/Sofa4.cpp
+++ Monocular Camera Design/ProceduralGeneration/Source/ProceduralGeneration/Sofa4.cpp
@@ -62,7 +62,7 @@ FString ASofa4::GetBoundingPointsOnScreen(int ResX, int ResY)
 
 	if (WasComponentRenderedRecently(RenderTime, SofaMesh))
 	{
-		BoxPositions += GetBoundingsOnScreen("Sofa", "0", SofaMesh, GetWorld(), ResX, ResY);
+		BoxPositions += GetBoundingsOnScreen("Sofa", "10", SofaMesh, GetWorld(), ResX, ResY);
 	}
 
 	if (WasComponentRenderedRecently(RenderTime, TableMesh))
