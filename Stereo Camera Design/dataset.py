9fdb6927cf526596bc5e8eb4576e6e10123a4bca
Oliver Yan <64511896+OYan60718@users.noreply.github.com>
2024-09-10 11:51:11 +1000

    Update dataset.py

--- Stereo Camera Design/dataset.py
+++ Stereo Camera Design/dataset.py
@@ -1,18 +1,45 @@
 # Data processing for the models 
+# The transform techniques are adopted from PSMNet implementation
 
 import numpy as np
+import random
 from torchvision import transforms
+import torch
+
+__imagenet_pca = {
+    'eigval': torch.Tensor([0.2175, 0.0188, 0.0045]),
+    'eigvec': torch.Tensor([
+        [-0.5675,  0.7192,  0.4009],
+        [-0.5808, -0.0045, -0.8140],
+        [-0.5836, -0.6948,  0.4203],
+    ])
+}
+
 
 # Process captured images, convert to tensors
 def get_item(left_img, right_img, disp):
-
     transform = transforms.Compose([
         transforms.ToPILImage(),
         transforms.ToTensor(),
+        ColorJitter(
+            brightness=0.4,
+            contrast=0.4,
+            saturation=0.4,
+        ),
+        Lighting(0.1, __imagenet_pca['eigval'], __imagenet_pca['eigvec']),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    dataL = np.ascontiguousarray(disp, dtype=np.float32)
+    h, w, _ = left_img.shape
+    th, tw = 256, 512
+
+    x1 = random.randint(0, w - tw)
+    y1 = random.randint(0, h - th)
+
+    left_img = left_img[y1:y1 + th, x1:x1 + tw, :]
+    right_img = right_img[y1:y1 + th, x1:x1 + tw, :]
+
+    disp = disp[y1:y1 + th, x1:x1 + tw]
 
     left_img = transform(left_img)
     right_img = transform(right_img)
@@ -20,6 +47,91 @@ def get_item(left_img, right_img, disp):
     transform = transforms.Compose([
         transforms.ToTensor()
     ])
-    dataL = transform(dataL)
+    disp = transform(disp)
 
-    return left_img, right_img, dataL
+    return left_img, right_img, disp
+
+
+class Lighting(object):
+    """Lighting noise(AlexNet - style PCA - based noise)"""
+    def __init__(self, alphastd, eigval, eigvec):
+        self.alphastd = alphastd
+        self.eigval = eigval
+        self.eigvec = eigvec
+
+    def __call__(self, img):
+        if self.alphastd == 0:
+            return img
+
+        alpha = img.new().resize_(3).normal_(0, self.alphastd)
+        rgb = self.eigvec.type_as(img).clone()\
+            .mul(alpha.view(1, 3).expand(3, 3))\
+            .mul(self.eigval.view(1, 3).expand(3, 3))\
+            .sum(1).squeeze()
+
+        return img.add(rgb.view(3, 1, 1).expand_as(img))
+
+
+class Grayscale(object):
+    def __call__(self, img):
+        gs = img.clone()
+        gs[0].mul_(0.299).add_(0.587, gs[1]).add_(0.114, gs[2])
+        gs[1].copy_(gs[0])
+        gs[2].copy_(gs[0])
+        return gs
+
+
+class Saturation(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        gs = Grayscale()(img)
+        alpha = random.uniform(0, self.var)
+        return img.lerp(gs, alpha)
+
+
+class Brightness(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        gs = img.new().resize_as_(img).zero_()
+        alpha = random.uniform(0, self.var)
+        return img.lerp(gs, alpha)
+
+
+class Contrast(object):
+    def __init__(self, var):
+        self.var = var
+
+    def __call__(self, img):
+        gs = Grayscale()(img)
+        gs.fill_(gs.mean())
+        alpha = random.uniform(0, self.var)
+        return img.lerp(gs, alpha)
+
+
+class RandomOrder(object):
+    """ Composes several transforms together in random order.
+    """
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img):
+        if self.transforms is None:
+            return img
+        order = torch.randperm(len(self.transforms))
+        for i in order:
+            img = self.transforms[i](img)
+        return img
+
+class ColorJitter(RandomOrder):
+    def __init__(self, brightness=0.4, contrast=0.4, saturation=0.4):
+        self.transforms = []
+        if brightness != 0:
+            self.transforms.append(Brightness(brightness))
+        if contrast != 0:
+            self.transforms.append(Contrast(contrast))
+        if saturation != 0:
+            self.transforms.append(Saturation(saturation))
